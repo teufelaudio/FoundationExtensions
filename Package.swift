597355087b9d48928d779be67a32d5789711d9fc
Luiz Rodrigo Martins Barbosa <luiz.barbosa@teufel.de>
2020-05-13 21:09:50 +0200

    Make the SPM package dynamic

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "FoundationExtensions",
     platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        .library(name: "FoundationExtensions", targets: ["FoundationExtensions"])
+        .library(name: "FoundationExtensions", type: .dynamic, targets: ["FoundationExtensions"])
     ],
     dependencies: [],
     targets: [
