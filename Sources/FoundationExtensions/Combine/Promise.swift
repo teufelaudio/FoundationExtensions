3043f050690660a5949267c50abf53c6d787ee46
Luiz Rodrigo Martins Barbosa <luiz.barbosa@teufel.de>
2020-05-30 22:55:41 +0200

    Add new run options for Promise

--- Sources/FoundationExtensions/Combine/Promise.swift
+++ Sources/FoundationExtensions/Combine/Promise.swift
@@ -98,12 +98,11 @@ extension Publishers.Promise {
                 .first()
                 .sink(
                     receiveCompletion: { [weak self] completion in
-                        if case let .failure(error) = completion {
-                            self?.downstream.receive(completion: .failure(error))
-                        }
+                        self?.downstream.receive(completion: completion)
                     },
                     receiveValue: { [weak self] value in
                         _ = self?.downstream.receive(value)
+                        self?.downstream.receive(completion: .finished)
                     }
                 )
         }
@@ -133,6 +132,42 @@ extension Publishers.Promise {
             },
             receiveValue: onSuccess
         )
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension Publishers.Promise where Failure == Never {
+    /// Similar to .sink, but with correct semantic for a single-value success or a failure. Creates demand for 1
+    /// value and completes after it, or on error.
+    /// - Parameters:
+    ///   - onSuccess: called when the promise is fulfilled successful with a value. Called only once and then
+    ///                you can consider this stream finished
+    /// - Returns: the subscription that can be cancelled at any point.
+    public func run(onSuccess: @escaping (Output) -> Void) -> AnyCancellable {
+        run(onSuccess: onSuccess, onFailure: { _ in })
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension Publishers.Promise where Success == Void {
+    /// Similar to .sink, but with correct semantic for a single-value success or a failure. Creates demand for 1
+    /// value and completes after it, or on error.
+    /// - Parameters:
+    ///   - onFailure: called when the promise finds error. Called only once completing the stream. It's never
+    ///                called if a success was already called
+    /// - Returns: the subscription that can be cancelled at any point.
+    public func run(onFailure: @escaping (Failure) -> Void) -> AnyCancellable {
+        run(onSuccess: { _ in }, onFailure: onFailure)
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension Publishers.Promise where Success == Void, Failure == Never {
+    /// Similar to .sink, but with correct semantic for a single-value success or a failure. Creates demand for 1
+    /// value and completes after it, or on error.
+    /// - Returns: the subscription that can be cancelled at any point.
+    public func run() -> AnyCancellable {
+        run(onSuccess: { _ in }, onFailure: { _ in })
     }
 }
 #endif
