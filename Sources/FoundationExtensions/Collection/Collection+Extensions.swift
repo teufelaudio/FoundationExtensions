1cdd2fae35fa45fc9ffc6b0e140b33fb9816cd94
Luiz Rodrigo Martins Barbosa <luiz.barbosa@teufel.de>
2020-06-09 13:05:09 +0200

    Move array extensions to collection

--- Sources/FoundationExtensions/Collection/Collection+Extensions.swift
+++ Sources/FoundationExtensions/Collection/Collection+Extensions.swift
@@ -68,3 +68,50 @@ extension Collection {
         }
     }
 }
+
+extension Collection {
+    /// Safely get the item at the given index. If the index is out-of-bounds, it will return nil
+    ///
+    /// - Parameter index: element index
+    /// - Returns: element at the given index or nil if wrong index was provided.
+    public subscript(safe index: Index) -> Element? {
+        get {
+            return inBounds(index) ? self[index] : nil
+        }
+    }
+
+    fileprivate func inBounds(_ index: Index) -> Bool {
+        return index < endIndex && index >= startIndex
+    }
+}
+
+extension MutableCollection {
+    /// Safely get or set the item at the given index. If the index is out-of-bounds, it will return nil
+    ///
+    /// - Parameter index: element index
+    /// - Returns: element at the given index or nil if wrong index was provided.
+    public subscript(safe index: Index) -> Element? {
+        get {
+            return inBounds(index) ? self[index] : nil
+        }
+        set {
+            guard inBounds(index), let newValue = newValue else { return }
+            self[index] = newValue
+        }
+    }
+}
+
+extension RangeReplaceableCollection {
+
+    /// Safely remove the item at the given index and return it.
+    /// If the index is out-of-bounds, array won't be modified and the return will be nil.
+    ///
+    /// - Parameter index: element index
+    /// - Returns: element removed or nil if wrong index was provided.
+    @discardableResult
+    public mutating func removeSafe(at index: Index) -> Element? {
+        guard let item = self[safe: index] else { return nil }
+        remove(at: index)
+        return item
+    }
+}
--- Tests/FoundationExtensionsTests/CollectionExtensionTests.swift
+++ Tests/FoundationExtensionsTests/CollectionExtensionTests.swift
@@ -198,4 +198,108 @@ class CollectionExtensionTests: XCTestCase {
         XCTAssertEqual(expectedResponseList, newList)
     }
 }
+
+extension CollectionExtensionTests {
+    func testSafeSubcriptGetSuccess() {
+        // given
+        let array = [1, 2, 3, 4]
+
+        // when
+        let sut = array[safe: 2]
+
+        // then
+        XCTAssertEqual(3, sut)
+    }
+
+    func testSafeSubcriptSetSuccess() {
+        // given
+        var array = [1, 2, 3, 4]
+
+        // when
+        array[safe: 2] = 42
+
+        // then
+        XCTAssertEqual([1, 2, 42, 4], array)
+    }
+
+    func testSafeSubcriptGetNegative() {
+        // given
+        let array = [1, 2, 3, 4]
+
+        // when
+        let sut = array[safe: -2]
+
+        // then
+        XCTAssertNil(sut)
+    }
+
+    func testSafeSubcriptSetNegative() {
+        // given
+        var array = [1, 2, 3, 4]
+
+        // when
+        array[safe: -2] = 42
+
+        // then
+        XCTAssertEqual([1, 2, 3, 4], array)
+    }
+
+    func testSafeSubcriptGetOutOfBounds() {
+        // given
+        let array = [1, 2, 3, 4]
+
+        // when
+        let sut = array[safe: 4]
+
+        // then
+        XCTAssertNil(sut)
+    }
+
+    func testSafeSubcriptSetOutOfBounds() {
+        // given
+        var array = [1, 2, 3, 4]
+
+        // when
+        array[safe: 4] = 42
+
+        // then
+        XCTAssertEqual([1, 2, 3, 4], array)
+    }
+
+    func testRemoveSafeSubcriptSuccess() {
+        // given
+        var array = [1, 2, 3, 4]
+
+        // when
+        let sut = array.removeSafe(at: 2)
+
+        // then
+        XCTAssertEqual([1, 2, 4], array)
+        XCTAssertEqual(3, sut)
+    }
+
+    func testRemoveSafeSubcriptNegative() {
+        // given
+        var array = [1, 2, 3, 4]
+
+        // when
+        let sut = array.removeSafe(at: -2)
+
+        // then
+        XCTAssertEqual([1, 2, 3, 4], array)
+        XCTAssertNil(sut)
+    }
+
+    func testRemoveSafeSubcriptOutOfBounds() {
+        // given
+        var array = [1, 2, 3, 4]
+
+        // when
+        let sut = array.removeSafe(at: 4)
+
+        // then
+        XCTAssertEqual([1, 2, 3, 4], array)
+        XCTAssertNil(sut)
+    }
+}
 #endif
