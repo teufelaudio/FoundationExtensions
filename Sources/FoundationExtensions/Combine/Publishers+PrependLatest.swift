c7d33e7f0c00648dafe0f579ffd52fb141f6301c
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2020-07-22 17:42:32 +0200

    Implement prependLatest

---
+++ Sources/FoundationExtensions/Combine/Publishers+PrependLatest.swift
@@ -0,0 +1,24 @@
+//
+//  Publishers+PrependLatest.swift
+//  
+//
+//  Created by Thomas Mellenthin on 22.07.20.
+//
+
+#if canImport(Combine)
+import Foundation
+import Combine
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension Publishers.CombineLatest {
+    public func prependLatest<P: Publisher>(_ publisher: P) -> AnyPublisher<Output, Failure> where P.Output == Output, P.Failure == Failure {
+        publisher.flatMap { left, right in
+            Publishers.CombineLatest(
+                self.a.prepend(left),
+                self.b.prepend(right)
+            )
+        }
+        .eraseToAnyPublisher()
+    }
+}
+#endif
---
+++ Tests/FoundationExtensionsTests/Combine/Publishers+PrependLatestTests.swift
@@ -0,0 +1,109 @@
+//
+//  Publishers+PrependLatestTests.m
+//  FoundationExtensionsTests
+//
+//  Created by Thomas Mellenthin on 22.07.20.
+//  Copyright © 2018 Lautsprecher Teufel GmbH. All rights reserved.
+//
+
+#if !os(watchOS)
+import FoundationExtensions
+import Combine
+import XCTest
+
+class PublishersPrependLatestTests: XCTestCase {
+}
+
+extension PublishersPrependLatestTests {
+
+    func testFirstEmptyThenBothvalue() {
+        // given
+        let first = PassthroughSubject<String, Never>()
+        let second = PassthroughSubject<String, Never>()
+        var events: [(String, String)] = []
+
+        let cancellable = Publishers.CombineLatest(first, second)
+            .prependLatest(Just(("empty", "empty")))
+            .sink { events.append($0) }
+
+        // when
+        first.send("a")
+        second.send("b")
+
+        // then
+        XCTAssertEqual(events.count, 3)
+        XCTAssertTrue(events[0] == ("empty", "empty"))
+        XCTAssertTrue(events[1] == ("a", "empty"))
+        XCTAssertTrue(events[2] == ("a", "b"))
+
+        // arc
+        cancellable.cancel()
+    }
+
+    func testFirstEmptyThenOnlyTheFirstValue() {
+        // given
+        let first = PassthroughSubject<String, Never>()
+        let second = PassthroughSubject<String, Never>()
+        var events: [(String, String)] = []
+
+        let cancellable = Publishers.CombineLatest(first, second)
+            .prependLatest(Just(("empty", "empty")))
+            .sink { events.append($0) }
+
+        // when - send only a
+        first.send("a")
+
+
+        // then
+        XCTAssertEqual(events.count, 2)
+        XCTAssertTrue(events[0] == ("empty", "empty"))
+        XCTAssertTrue(events[1] == ("a", "empty"))
+
+        // arc
+        cancellable.cancel()
+    }
+
+    func testFirstEmptyThenOnlyTheSecondValue() {
+        // given
+        let first = PassthroughSubject<String, Never>()
+        let second = PassthroughSubject<String, Never>()
+        var events: [(String, String)] = []
+
+        let cancellable = Publishers.CombineLatest(first, second)
+            .prependLatest(Just(("empty", "empty")))
+            .sink { events.append($0) }
+
+        // when - send only b
+        second.send("b")
+
+
+        // then
+        XCTAssertEqual(events.count, 2)
+        XCTAssertTrue(events[0] == ("empty", "empty"))
+        XCTAssertTrue(events[1] == ("empty", "b"))
+
+        // arc
+        cancellable.cancel()
+    }
+
+    func testFirstEmptyAndNoOtherEvents() {
+        // given
+        let first = PassthroughSubject<String, Never>()
+        let second = PassthroughSubject<String, Never>()
+        var events: [(String, String)] = []
+
+        let cancellable = Publishers.CombineLatest(first, second)
+            .prependLatest(Just(("empty", "empty")))
+            .sink { events.append($0) }
+
+        // when - do nothing
+
+        // then
+        XCTAssertEqual(events.count, 1)
+        XCTAssertTrue(events[0] == ("empty", "empty"))
+
+        // arc
+        cancellable.cancel()
+    }
+}
+#endif
