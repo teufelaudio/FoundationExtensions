0089c10c57d4fd06669eeed876325680afe67090
Thomas Mellenthin <github@dysternis.de>
2020-08-18 18:30:26 +0200

    CombineLatest6 (#14)

--- Sources/FoundationExtensions/Combine/Publishers+CombineLatest.swift
+++ Sources/FoundationExtensions/Combine/Publishers+CombineLatest.swift
@@ -46,6 +46,45 @@ extension Publishers {
                 .receive(subscriber: subscriber)
         }
     }
+
+    public struct CombineLatest6<A, B, C, D, E, F>: Publisher where A: Publisher,
+                                                                 B: Publisher,
+                                                                 C: Publisher,
+                                                                 D: Publisher,
+                                                                 E: Publisher,
+                                                                 F: Publisher,
+                                                                 A.Failure == B.Failure,
+                                                                 B.Failure == C.Failure,
+                                                                 C.Failure == D.Failure,
+                                                                 D.Failure == E.Failure,
+                                                                 E.Failure == F.Failure{
+        public typealias Output = (A.Output, B.Output, C.Output, D.Output, E.Output, F.Output)
+        public typealias Failure = A.Failure
+
+        let a: A
+        let b: B
+        let c: C
+        let d: D
+        let e: E
+        let f: F
+
+        public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F) {
+            self.a = a
+            self.b = b
+            self.c = c
+            self.d = d
+            self.e = e
+            self.f = f
+        }
+
+        public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+            Publishers.CombineLatest(Publishers.CombineLatest5(a, b, c, d, e), f)
+                .map { (abcdeTuple, fOutput) in
+                    (abcdeTuple.0, abcdeTuple.1, abcdeTuple.2, abcdeTuple.3, abcdeTuple.4, fOutput)
+                }
+                .receive(subscriber: subscriber)
+        }
+    }
 }
 
 #endif
