b537f96967d63f68c8aa7053d4e746139990fa21
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2020-08-18 18:22:28 +0200

    CombineLatest6

--- Tests/FoundationExtensionsTests/Combine/Publishers+CombineLatestTests.swift
+++ Tests/FoundationExtensionsTests/Combine/Publishers+CombineLatestTests.swift
@@ -17,7 +17,7 @@ class PublishersCombineLatestTests: XCTestCase {
 extension PublishersCombineLatestTests {
 
     @available(iOS 13.0, *)
-    func testFirstEmptyThenBothvalue() {
+    func testCombineLatest5() {
         // given
         let a = PassthroughSubject<String, Never>()
         let b = PassthroughSubject<String, Never>()
@@ -46,6 +46,43 @@ extension PublishersCombineLatestTests {
         XCTAssertTrue(events[1] == ("a", "bb",  "c",  "d", "e"))
         XCTAssertTrue(events[2] == ("a", "bb",  "cc", "d", "e"))
         XCTAssertTrue(events[3] == ("a", "bbb", "cc", "d", "e"))
+
+        // arc
+        cancellable.cancel()
+    }
+
+    @available(iOS 13.0, *)
+    func testCombineLatest6() {
+        // given
+        let a = PassthroughSubject<String, Never>()
+        let b = PassthroughSubject<String, Never>()
+        let c = PassthroughSubject<String, Never>()
+        let d = PassthroughSubject<String, Never>()
+        let e = PassthroughSubject<String, Never>()
+        let f = PassthroughSubject<String, Never>()
+        var events: [(String, String, String, String, String, String)] = []
+
+        let cancellable = Publishers.CombineLatest6(a, b, c, d, e, f)
+            .sink { events.append($0) }
+
+        // when
+        a.send("a")
+        b.send("b")
+        c.send("c")
+        d.send("d")
+        e.send("e")
+        f.send("f")
+
+        b.send("bb")
+        c.send("cc")
+        b.send("bbb")
+
+        // then
+        XCTAssertEqual(events.count, 4)
+        XCTAssertTrue(events[0] == ("a", "b",   "c",  "d", "e", "f"))
+        XCTAssertTrue(events[1] == ("a", "bb",  "c",  "d", "e", "f"))
+        XCTAssertTrue(events[2] == ("a", "bb",  "cc", "d", "e", "f"))
+        XCTAssertTrue(events[3] == ("a", "bbb", "cc", "d", "e", "f"))
 
         // arc
         cancellable.cancel()
