e8fefc04e53efc259bc09471c5bea89b48520cde
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2022-07-05 19:17:43 +0200

    Adds Data(hex:) to allow creating Data from hex-Strings (without prefix)

--- Sources/FoundationExtensions/Data/Data+Extensions.swift
+++ Sources/FoundationExtensions/Data/Data+Extensions.swift
@@ -33,6 +33,25 @@ extension Numeric {
 
 extension Data {
 
+    public init?(hex: String) {
+        let length = hex.count / 2
+        var data = Data(capacity: length)
+        for i in 0 ..< length {
+            let j = hex.index(hex.startIndex, offsetBy: i * 2)
+            let k = hex.index(j, offsetBy: 2)
+            let bytes = hex[j..<k]
+            if var byte = UInt8(bytes, radix: 16) {
+                data.append(&byte, count: 1)
+            } else {
+                return nil
+            }
+        }
+        self = data
+    }
+}
+
+extension Data {
+
     public func range(start: Int, length: Int) -> Data {
         guard start < endIndex else { return Data() }
         let end = Swift.min(endIndex, start + length)
