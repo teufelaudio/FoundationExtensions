9eacde8cf88dc07eea09bbe4c9905e8ba5e75276
Giulia Ariu <37103218+GiuliaAriu@users.noreply.github.com>
2022-10-04 20:50:29 +0200

    Interpolated CGFloat (#37)
    Co-authored-by: Giulia Ariu <>

---
+++ Sources/FoundationExtensions/CoreGraphics/CGFloat+Extensions.swift
@@ -0,0 +1,22 @@
+// Copyright © 2022 Lautsprecher Teufel GmbH. All rights reserved.
+
+import CoreGraphics
+
+extension CGFloat {
+    private func transform(
+        fromLowerBound: Self,
+        fromUpperBound: Self,
+        toLowerBound: Self,
+        toUpperBound: Self) -> Self {
+        let positionInRange = (self - fromLowerBound) / (fromUpperBound - fromLowerBound)
+        return (positionInRange * (toUpperBound - toLowerBound)) + toLowerBound
+    }
+
+    public func trasformation(from: ClosedRange<Self>, to: ClosedRange<Self>) -> Self {
+        transform(
+            fromLowerBound: from.lowerBound,
+            fromUpperBound: from.upperBound,
+            toLowerBound: to.lowerBound,
+            toUpperBound: to.upperBound)
+    }
+}
