cc8e39be73275a77e73b603099ae6297b96bfb81
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2022-11-24 12:50:59 +0100

    Add L10n
    Have all localisation components at one place.

---
+++ Sources/FoundationExtensions/L10n/Localizable.swift
@@ -0,0 +1,22 @@
+// Copyright © 2022 Lautsprecher Teufel GmbH. All rights reserved.
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public protocol Localizable {
+    var key: LocalizedStringKey { get }
+    var rootKey: String { get }
+}
+
+@available(iOS 13.0, *)
+extension Localizable {
+    public var key: LocalizedStringKey {
+        .init(
+            "\(rootKey).\(Mirror(reflecting: self).children.first?.label ?? "\(self)")"
+        )
+    }
+
+    public var rootKey: String {
+        String(reflecting: Self.self)
+    }
+}
