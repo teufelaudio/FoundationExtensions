456bf7d8806cecb36668a7533fe8a3ef19d6f1a7
Oguz Yuksel <oguz.yueksel@teufel.de>
2023-03-01 13:29:19 +0100

    improve functionaly and documentation

--- Sources/FoundationExtensions/Utils/MutableParameter.swift
+++ Sources/FoundationExtensions/Utils/MutableParameter.swift
@@ -15,12 +15,48 @@ public enum MutableParameter<T> {
     case value(T)
     case changing(old: T, new: T)
 
-    public func requestChange(to newValue: T) -> MutableParameter<T> {
+    public func requestChange(to newValue: T) -> Self {
         return .changing(old: pessimisticValue, new: newValue)
     }
 
     public mutating func requestChangeMutating(to newValue: T) {
         self = self.requestChange(to: newValue)
+    }
+
+    /// - Returns: The new `.value(_)` if self is currently changing. Otherwise, returns nil.
+    public func successRequest() -> Self? {
+        switch self {
+        case .value:
+            return nil
+        case let .changing(old: _, new: value):
+            return .value(value)
+        }
+    }
+
+    /// - Returns: `.value(newValue)` if self is successfully mutated. Otherwise returns `nil`.
+    @discardableResult
+    public mutating func successRequestMutating() -> Self? {
+        guard let result = successRequest() else { return nil }
+        self = result
+        return self
+    }
+
+    /// - Returns: The old `.value(_)` if self is currently changing. Otherwise, returns nil.
+    public func failRequest() -> Self? {
+        switch self {
+        case .value:
+            return nil
+        case let .changing(old: value, new: _):
+            return .value(value)
+        }
+    }
+
+    /// - Returns: `.value(oldValue)` if self is successfully mutated. Otherwise returns `nil`.
+    @discardableResult
+    public mutating func failRequestMutating() -> Self? {
+        guard let result = failRequest() else { return nil }
+        self = result
+        return self
     }
 
     /// - Returns: The current value or the new one.
---
+++ Tests/FoundationExtensionsTests/MutableParameterTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Oguz Yüksel on 01.03.23.
+//
+
+import Foundation
