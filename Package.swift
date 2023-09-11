1a129f24885cbc670d9437becd87b80d665c2e52
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2023-09-11 17:55:20 +0200

    Add runtimeWarn() as public function (#51)
    * Add runtimeWarn() as public function
    
    Unfortunately without XCTest support
    
    * fix failing tests by hard-coding the language to english

--- Package.swift
+++ Package.swift
@@ -3,15 +3,29 @@ import PackageDescription
 
 let package = Package(
     name: "FoundationExtensions",
-    platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "FoundationExtensions", targets: ["FoundationExtensions"]),
         .library(name: "FoundationExtensionsDynamic", type: .dynamic, targets: ["FoundationExtensions"])
     ],
-    dependencies: [],
+    dependencies: [
+    ],
     targets: [
-        .target(name: "FoundationExtensions", dependencies: []),
-        .target(name: "FoundationExtensionsDynamic", dependencies: []),
-        .testTarget(name: "FoundationExtensionsTests", dependencies: ["FoundationExtensions"])
+        .target(
+            name: "FoundationExtensions",
+            dependencies: [
+            ]
+        ),
+        .target(
+            name: "FoundationExtensionsDynamic",
+            dependencies: [
+            ]
+        ),
+        .testTarget(
+            name: "FoundationExtensionsTests",
+            dependencies: [
+                "FoundationExtensions"
+            ]
+        )
     ]
 )
